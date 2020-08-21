ac07bcae0fa86e009ba28f553849025d1ae4e75a
oom <venom0218@gmail.com>
2020-08-22 00:02:42 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// automatically pick platform
+const Say = require('say');
+
+(async () => {
+    // Use default system voice and speed
+    
+    await Say.speak('Welcome back sir!');
+    
+})();
+
